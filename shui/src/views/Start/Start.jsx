ab2c5cf54e481153f8824c4cf6a19f6a4fdca639
Josefine Forsman <josefineforsman1@gmail.com>
2023-05-23 16:21:17 +0200

    start page changes

--- shui/src/views/Start/Start.jsx
+++ shui/src/views/Start/Start.jsx
@@ -1,9 +1,15 @@
 import "./Start.css";
+import top from "../../assets/top.png";
 
 function Start() {
   return (
-    <section>
-      <p>Start</p>
+    <section data-id="start" className="start">
+      <img data-id="logo" className="logo start__logo" src={top} alt="logo" />
+      <article className="start__title--container">
+        <h1 data-id="start__title" className="start__title">
+          Du har inga meddelanden att visa.
+        </h1>
+      </article>
     </section>
   );
 }
