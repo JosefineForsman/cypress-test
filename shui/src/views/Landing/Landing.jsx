107b7e33d457fbe9521f20134abcda1c7f630723
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2023-05-23 16:22:32 +0200

    starting w landing

--- shui/src/views/Landing/Landing.jsx
+++ shui/src/views/Landing/Landing.jsx
@@ -1,9 +1,20 @@
 import "./Landing.css";
+import footer from '../../assets/footer.svg'
+
 
 function Landing() {
   return (
-    <section>
-      <p>Landing</p>
+    <section >
+      {/* <img data-id='logo' className="logo landing__logo" src= { } alt="" /> */}
+        <article data-id='landing' className="landing">
+          <figure className="landing__logo"></figure>
+          <article className="landing__btn--container">
+            <button data-id='landing__btn' className="landing__btn">Skriv en kommentar</button>
+          </article>
+          <img src= { footer } alt="" />
+       </article>
+      
+   
     </section>
   );
 }
