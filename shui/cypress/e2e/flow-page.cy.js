875c420c369586ee41909344f71d7e8b371b4071
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2023-05-24 15:05:53 +0200

    Last testing in Flow

--- shui/cypress/e2e/flow-page.cy.js
+++ shui/cypress/e2e/flow-page.cy.js
@@ -7,14 +7,11 @@ describe('template spec', () => {
   it(' should be able to click on the Pen icon to be navigated to A new message.', () => {
     cy.get('[data-id="flow__pen"]').should('exist').click()
     cy.get('[data-id="message"]').should('be.visible');
-
   });
 
   it('In each message there must be a date, text message and a username', () => {
-    // cy.get('[data-id="flow__date"]').should('be.visible')
+    cy.get('[data-id="flow__date"]').should('be.visible')
     cy.get('[data-id="flow__text"]').should('be.visible')
     cy.get('[data-id="flow__username"]').should('be.visible')
-
-
   });
 })
