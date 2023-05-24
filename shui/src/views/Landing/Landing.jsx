053add75e42930f3d4e43449540b296f99ceeec8
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2023-05-24 09:50:09 +0200

    Css Landing done! Css Message 90% done

--- shui/src/views/Landing/Landing.jsx
+++ shui/src/views/Landing/Landing.jsx
@@ -1,20 +1,18 @@
 import "./Landing.css";
 import footer from '../../assets/footer.svg'
+import top from '../../assets/top.png'
 
 
 function Landing() {
   return (
     <section >
-      {/* <img data-id='logo' className="logo landing__logo" src= { } alt="" /> */}
         <article data-id='landing' className="landing">
-          <figure className="landing__logo"></figure>
+         <img data-id='logo' className="logo landing__logo" src= { top } alt="" />
           <article className="landing__btn--container">
             <button data-id='landing__btn' className="landing__btn">Skriv en kommentar</button>
           </article>
           <img src= { footer } alt="" />
        </article>
-      
-   
     </section>
   );
 }
