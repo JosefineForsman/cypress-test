461678c70d6f384632be83446f00296034ecb1b6
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2023-05-24 14:07:54 +0200

    testing in Landing and Message page

---
+++ shui/cypress/e2e/flow-page.cy.js
@@ -0,0 +1,16 @@
+describe('template spec', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:5175/flow')
+    cy.viewport(1000, 1000);
+  })
+
+  it(' should be able to click on the Pen icon to be navigated to A new message.', () => {
+
+
+  });
+
+  it('In each message there must be a date, text message and a username', () => {
+
+
+  });
+})
