a2069296094cf5ebf7f4c178e1b028db96b191a9
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2023-05-24 10:12:38 +0200

    starting function in message

--- shui/src/views/Message/Message.jsx
+++ shui/src/views/Message/Message.jsx
@@ -1,8 +1,21 @@
 import "./Message.css";
 import top from '../../assets/top.png';
 import footer from '../../assets/footer.svg';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Message() {
+  const navigate = useNavigate()
+  const {text, setText} = useState()
+  const {username, setUsername} = useState()
+
+
+  function handleClick() {
+    navigate('/flow')
+  }
+
+
+
   return (
     <section >
     <article data-id='message' className="message">
@@ -10,7 +23,7 @@ function Message() {
       <textarea data-id='message__text' className="message__text" cols="45" rows="20" placeholder="Vi är grymma!"></textarea>
     <input data-id='message__input' className="message__input" type="text" placeholder=" Användarnamn" />
       <article className="message__btn--container">
-        <button data-id='message__btn' className="message__btn">Publicera</button>
+        <button data-id='message__btn' className="message__btn" onClick={ handleClick }>Publicera</button>
       </article>
       <img src= { footer } alt="" />
    </article>
