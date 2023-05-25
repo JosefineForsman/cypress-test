3a4ede5d11e2237322a2ec6928b836eaa94599ee
Josefine Forsman <josefineforsman1@gmail.com>
2023-05-25 09:07:23 +0200

    correct navigation

--- shui/src/views/Landing/Landing.jsx
+++ shui/src/views/Landing/Landing.jsx
@@ -1,26 +1,31 @@
 import "./Landing.css";
-import footer from '../../assets/footer.svg'
-import top from '../../assets/top.svg'
+import footer from "../../assets/footer.svg";
+import top from "../../assets/top.svg";
 
 import { useNavigate } from "react-router-dom";
 
 function Landing() {
   const navigate = useNavigate();
 
-
   function handleClick() {
-    navigate('/message')
+    navigate("/start");
   }
 
   return (
-    <section >
-        <article data-id='landing' className="landing">
-         <img data-id='logo' className="logo landing__logo" src= { top } alt="" />
-          <article className="landing__btn--container">
-            <button data-id='landing__btn' className="landing__btn" onClick={handleClick}>Skriv en kommentar</button>
-          </article>
-          <img src= { footer } alt="" />
-       </article>
+    <section>
+      <article data-id="landing" className="landing">
+        <img data-id="logo" className="logo landing__logo" src={top} alt="" />
+        <article className="landing__btn--container">
+          <button
+            data-id="landing__btn"
+            className="landing__btn"
+            onClick={handleClick}
+          >
+            Skriv en kommentar
+          </button>
+        </article>
+        <img src={footer} alt="" />
+      </article>
     </section>
   );
 }
--- shui/src/views/Start/Start.jsx
+++ shui/src/views/Start/Start.jsx
@@ -1,19 +1,40 @@
 import "./Start.css";
+import { useNavigate } from "react-router-dom";
 import top from "../../assets/top.svg";
 import pen from "../../assets/pen.svg";
 import footer from "../../assets/footer.svg";
 function Start() {
+  const navigate = useNavigate();
+
+  function navigateToMessage() {
+    navigate("/message");
+  }
+
+  function navigateToHome() {
+    navigate("/");
+  }
   return (
     <section data-id="start" className="start">
       <article className="start__container">
-        <img data-id="logo" className="logo start__logo" src={top} alt="logo" />
+        <img
+          data-id="logo"
+          className="logo start__logo"
+          src={top}
+          alt="logo"
+          onClick={navigateToHome}
+        />
         <article className="start__title--container">
           <h1 data-id="start__title" className="start__title">
             Du har inga meddelanden att visa.
           </h1>
         </article>
         <footer data-id="footer" className="start__footer">
-          <img src={pen} alt="" className="footer footer__pen" />
+          <img
+            src={pen}
+            alt=""
+            className="footer footer__pen"
+            onClick={navigateToMessage}
+          />
           <img src={footer} alt="" className="footer__background" />
         </footer>
       </article>
